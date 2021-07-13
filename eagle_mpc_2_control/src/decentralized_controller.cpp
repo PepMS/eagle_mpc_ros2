2489c6fd3c23278ea241bb3da792c525f6e87acb
PepMS <jmarti@iri.upc.edu>
2021-07-13 11:53:06 +0200

    [rearranged] repo rearranged and renamed

--- eagle_mpc_2_control/src/decentralized_controller.cpp
+++ eagle_mpc_2_control/src/decentralized_controller.cpp
@@ -1,31 +1,31 @@
-#include "px4_mpc/offboard_control.hpp"
+#include "eagle_mpc_2_control/decentralized_controller.hpp"
 
 using namespace std::chrono_literals;
 
-OffboardControl::OffboardControl(rclcpp::Node::SharedPtr node) : node_(node) {
+DecentralizedController::DecentralizedController(rclcpp::Node::SharedPtr node) : node_(node) {
   offboard_control_mode_publisher_ =
       node_->create_publisher<px4_msgs::msg::OffboardControlMode>("OffboardControlMode_PubSubTopic", 10);
   trajectory_setpoint_publisher_ =
       node_->create_publisher<px4_msgs::msg::TrajectorySetpoint>("TrajectorySetpoint_PubSubTopic", 10);
   vehicle_command_publisher_ =
       node_->create_publisher<px4_msgs::msg::VehicleCommand>("VehicleCommand_PubSubTopic", 10);
-  platform_state_publisher_ = node_->create_publisher<eagle_interfaces::msg::PlatformState>("PlatformState", 10);
+  platform_state_publisher_ = node_->create_publisher<eagle_mpc_2_msgs::msg::PlatformState>("PlatformState", 10);
 
   timesync_sub_ = node_->create_subscription<px4_msgs::msg::Timesync>(
-      "Timesync_PubSubTopic", 10, std::bind(&OffboardControl::timeSyncCallback, this, std::placeholders::_1));
+      "Timesync_PubSubTopic", 10, std::bind(&DecentralizedController::timeSyncCallback, this, std::placeholders::_1));
   local_position_subs_ = node_->create_subscription<px4_msgs::msg::VehicleLocalPosition>(
       "VehicleLocalPosition_PubSubTopic", 10,
-      std::bind(&OffboardControl::vehicleLocalPositionCallback, this, std::placeholders::_1));
+      std::bind(&DecentralizedController::vehicleLocalPositionCallback, this, std::placeholders::_1));
   attitude_subs_ = node_->create_subscription<px4_msgs::msg::VehicleAttitude>(
       "VehicleAttitude_PubSubTopic", 10,
-      std::bind(&OffboardControl::vehicleAttitudeCallback, this, std::placeholders::_1));
+      std::bind(&DecentralizedController::vehicleAttitudeCallback, this, std::placeholders::_1));
   angular_velocity_subs_ = node_->create_subscription<px4_msgs::msg::VehicleAngularVelocity>(
       "VehicleAngularVelocity_PubSubTopic", 10,
-      std::bind(&OffboardControl::vehicleAngularVelocityCallback, this, std::placeholders::_1));
+      std::bind(&DecentralizedController::vehicleAngularVelocityCallback, this, std::placeholders::_1));
 
   offboard_setpoint_counter_ = 0;
-  timer_ = node_->create_wall_timer(100ms, std::bind(&OffboardControl::timerCallback, this));
-  timer_wp_ = node_->create_wall_timer(5s, std::bind(&OffboardControl::timerWpCallback, this));
+  timer_ = node_->create_wall_timer(100ms, std::bind(&DecentralizedController::timerCallback, this));
+  timer_wp_ = node_->create_wall_timer(5s, std::bind(&DecentralizedController::timerWpCallback, this));
 
   // Load wps
   px4_msgs::msg::TrajectorySetpoint wp;
@@ -59,13 +59,13 @@ OffboardControl::OffboardControl(rclcpp::Node::SharedPtr node) : node_(node) {
   state_ = Eigen::VectorXd::Zero(13);
 }
 
-OffboardControl::~OffboardControl() {}
+DecentralizedController::~DecentralizedController() {}
 
-void OffboardControl::timeSyncCallback(const px4_msgs::msg::Timesync::UniquePtr msg) {
+void DecentralizedController::timeSyncCallback(const px4_msgs::msg::Timesync::UniquePtr msg) {
   timestamp_.store(msg->timestamp);
 }
 
-void OffboardControl::timerCallback() {
+void DecentralizedController::timerCallback() {
   if (offboard_setpoint_counter_ == 10) {
     // Change to Offboard mode after 10 setpoints
     this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
@@ -84,24 +84,24 @@ void OffboardControl::timerCallback() {
   }
 }
 
-void OffboardControl::timerWpCallback() {
+void DecentralizedController::timerWpCallback() {
   waypoint_active_++;
   waypoint_active_ = waypoint_active_ == waypoints_.size() ? waypoints_.size() - 1 : waypoint_active_;
 }
 
-void OffboardControl::arm() const {
+void DecentralizedController::arm() const {
   publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
 
   RCLCPP_INFO(node_->get_logger(), "Arm command send");
 }
 
-void OffboardControl::disarm() const {
+void DecentralizedController::disarm() const {
   publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
 
   RCLCPP_INFO(node_->get_logger(), "Disarm command send");
 }
 
-void OffboardControl::publish_offboard_control_mode() const {
+void DecentralizedController::publish_offboard_control_mode() const {
   px4_msgs::msg::OffboardControlMode msg{};
   msg.timestamp = timestamp_.load();
   msg.position = true;
@@ -113,7 +113,7 @@ void OffboardControl::publish_offboard_control_mode() const {
   offboard_control_mode_publisher_->publish(msg);
 }
 
-void OffboardControl::publish_trajectory_setpoint() const {
+void DecentralizedController::publish_trajectory_setpoint() const {
   px4_msgs::msg::TrajectorySetpoint msg{};
   msg.timestamp = timestamp_.load();
   msg.x = waypoints_[waypoint_active_].x;
@@ -124,7 +124,7 @@ void OffboardControl::publish_trajectory_setpoint() const {
   trajectory_setpoint_publisher_->publish(msg);
 }
 
-void OffboardControl::publish_vehicle_command(uint16_t command, float param1, float param2) const {
+void DecentralizedController::publish_vehicle_command(uint16_t command, float param1, float param2) const {
   px4_msgs::msg::VehicleCommand msg{};
   msg.timestamp = timestamp_.load();
   msg.param1 = param1;
@@ -139,7 +139,7 @@ void OffboardControl::publish_vehicle_command(uint16_t command, float param1, fl
   vehicle_command_publisher_->publish(msg);
 }
 
-void OffboardControl::vehicleLocalPositionCallback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr msg) {
+void DecentralizedController::vehicleLocalPositionCallback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr msg) {
   // We use the local inertial frame: NWU
   // Position in the inertial frame
   state_(0) = msg->x;
@@ -156,7 +156,7 @@ void OffboardControl::vehicleLocalPositionCallback(const px4_msgs::msg::VehicleL
   state_(9) = -vel_frd_(2);
 }
 
-void OffboardControl::vehicleAttitudeCallback(const px4_msgs::msg::VehicleAttitude::UniquePtr msg) {
+void DecentralizedController::vehicleAttitudeCallback(const px4_msgs::msg::VehicleAttitude::UniquePtr msg) {
   q_ned_frd_ = px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
   q_nwu_flu_ = NWU_NED_Q * q_ned_frd_ * FRD_FLU_Q;
   state_(3) = q_nwu_flu_.x();
@@ -165,7 +165,7 @@ void OffboardControl::vehicleAttitudeCallback(const px4_msgs::msg::VehicleAttitu
   state_(6) = q_nwu_flu_.w();
 }
 
-void OffboardControl::vehicleAngularVelocityCallback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr msg) {
+void DecentralizedController::vehicleAngularVelocityCallback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr msg) {
   state_(10) = msg->xyz[0];
   state_(11) = -msg->xyz[1];
   state_(12) = -msg->xyz[2];
@@ -173,7 +173,7 @@ void OffboardControl::vehicleAngularVelocityCallback(const px4_msgs::msg::Vehicl
   publish_platform_state();
 }
 
-void OffboardControl::publish_platform_state() {
+void DecentralizedController::publish_platform_state() {
   // platform_state_msg_.header.frame_id = 0
   platform_state_msg_.header.stamp = node_->now();
 
@@ -199,7 +199,7 @@ int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
 
   auto n = rclcpp::Node::make_shared("mpc_runner");
-  std::shared_ptr<OffboardControl> controller = std::make_shared<OffboardControl>(n);
+  std::shared_ptr<DecentralizedController> controller = std::make_shared<DecentralizedController>(n);
 
   rclcpp::spin(n);
   rclcpp::shutdown();
